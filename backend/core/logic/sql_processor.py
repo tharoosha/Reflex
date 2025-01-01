d38eb39b685f739833536d8499a4a069a3e9e60e
tharoosha <tharooshavihidun@gmail.com>
2025-01-01 11:38:36 +0530

    conflict resolve

--- backend/core/logic/sql_processor.py
+++ backend/core/logic/sql_processor.py
@@ -4,7 +4,7 @@ from core.llm_services.mistral_api import MistralAPI
 
 
 class SQLProcessor:
-    def __init__(self, tool_names: list = None, enable_tools=False):
+    def __init__(self, tool_names: list = None, enable_tools=False, prompt_template: str = None):
         """
         Initialize the NLPProcessor with optional tool support.
 
@@ -12,12 +12,14 @@ class SQLProcessor:
         """
         mistral_api = MistralAPI()
         mistral_id, mistral_api_key = mistral_api.getNameAndAPIKey()
+
+        prompt_template = prompt_template if prompt_template else ""
         
         if enable_tools:
             # Initialize tools dynamically based on provided tool names
             tools = ToolRegistry.get_tools(tool_names) if tool_names else []
 
-            self.nlp_agent = MistralAgent(mistral_id, mistral_api_key, prompt_template="", tools=tools)
+            self.sql_agent = MistralAgent(mistral_id, mistral_api_key, prompt_template= prompt_template, tools=tools)
 
     def process_input(self, nl_input: str) -> str:
         """
@@ -27,4 +29,4 @@ class SQLProcessor:
         :return: The agent's response.
         """
         prompt = f"Process this input: {nl_input}"
-        return self.nlp_agent.generate(prompt)
+        return self.sql_agent.generate(prompt)
